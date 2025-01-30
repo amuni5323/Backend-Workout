8930fd02a02234c0a83342499f4fa066a78f3d6d
amuni5323 <amuni11.hamai@gmail.com>
2025-01-30 21:55:41 +0300

    Updated CORS with frontend URL

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://frontend-workout-6ygx.vercel.app", // ✅ Allow only this frontend
+  methods: "GET, POST, PUT, DELETE",
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
